f702d2817ac728e63174b018e98eba65250fdae5
Aleix <aleixcoma@gmail.com>
2023-08-16 21:50:27 +0200

    Implemented functions

--- normal_stresses_for_beams_in_bending.py
+++ normal_stresses_for_beams_in_bending.py
@@ -9,11 +9,12 @@ import math
 # 6. Beam proportions are such that it would fail by bending rather than crushing, wrinkling, or sidewise buckling.
 # 7. Plane cross sections of the beam remain plane during bending.
 
-#M = float(input("Enter bending moment[N·m]: "))
-#y = float(input("Enter distance from the neutral axis[m]: "))
-#I = float(input("Enter second-area moment about the z axis[m^4]: ")) # x axis is the neutral/centroidal axis. x-z is the neutral plane
-#sigma_x = -M*y/I # Stress in the direction of the neutral axis. Can be either positive (tension) or negative (compression)
-#print(sigma_x)
+def pure_bending_tension():
+    M = float(input("Enter bending moment[N·m]: "))
+    y = float(input("Enter distance from the neutral axis[m]: "))
+    I = float(input("Enter second-area moment about the z axis[m^4]: ")) # x axis is the neutral/centroidal axis. x-z is the neutral plane
+    sigma_x = M*y/I # Stress in the direction of the neutral axis. Can be either positive (tension) or negative (compression)
+    print("Stress in the direction of the neutral axis[MPa]: ", sigma_x*1e-6)
 
 # Two plane bending stresses. Assume:
 
@@ -22,19 +23,35 @@ import math
 # Previously, determine maximum bending stresses (M_z & M_y), distances from the neutral axis (z & y)
 # and second-area moments (I_z & I_y).
 
-#M_z = float(input("Enter bending moment in the xy plane[lbf·in]: "))
-#M_y = float(input("Enter bending moment in the xz plane[lbf·in]: "))
-#z = float(input("Enter distance from the neutral axis in the xz plane[in]: "))
-#y = float(input("Enter distance from the neutral axis in the xy plane[in]: "))
-#I_z = float(input("Enter second-area moment around the z axis[in^4]: "))
-#I_y = float(input("Enter second-area moment around the y axis[in^4]: "))
-#sigma_x = -M_z*y/I_z + M_y*z/I_y
-#print(sigma_x)
+def two_plane_bending_rectangular_section_tension():
+    M_z = float(input("Enter bending moment in the xy plane[lbf·in]: "))
+    M_y = float(input("Enter bending moment in the xz plane[lbf·in]: "))
+    z = float(input("Enter distance from the neutral axis in the z direction[in]: "))
+    y = float(input("Enter distance from the neutral axis in the y direction[in]: "))
+    I_z = float(input("Enter second-area moment around the z axis[in^4]: "))
+    I_y = float(input("Enter second-area moment around the y axis[in^4]: "))
+    sigma_x = -M_z*y/I_z + M_y*z/I_y
+    print("Stress in the direction of the neutral axis[kpsi]: ", sigma_x)
 
 # Circular cross section:
 
-M_z = float(input("Enter bending moment in the xy plane[lbf·in]: "))
-M_y = float(input("Enter bending moment in the xz plane[lbf·in]: "))
-d = float(input("Enter diameter of the circular cross section [in]: "))
-sigma_m = 32/(math.pi*d**3)*math.sqrt(M_y**2+M_z**2)
-print(sigma_m)
+def two_plane_bending_circular_section_tension():
+    M_z = float(input("Enter bending moment in the xy plane[lbf·in]: "))
+    M_y = float(input("Enter bending moment in the xz plane[lbf·in]: "))
+    d = float(input("Enter diameter of the circular cross section [in]: "))
+    sigma_m = 32/(math.pi*d**3)*math.sqrt(M_y**2+M_z**2)
+    print("Maximum stress in the direction of the neutral axis[kpsi]: ", sigma_m)
+
+print("Select calculation: \n")
+print("1: Tension-compression in a rectangular cross-section beam subjected to pure bending\n")
+print("2: Tension-compression in a rectangular cross-section beam subjected to two-plane bending\n")
+print("3: Tension-compression in a circular cross-section beam subjected to two-plane bending\n")
+select = float(input("Introduce number: "))
+if select==1:
+    pure_bending_tension()
+elif select==2:
+    two_plane_bending_rectangular_section_tension()
+elif select==3:
+    two_plane_bending_circular_section_tension()
+else:
+    print("Selection is not valid")
